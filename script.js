f85183201e7af119576b5b954da87a7960d22c43
Joanna Stiller <joannastiller@yahoo.com>
2019-01-15 21:35:24 +0100

    JS for the converter

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('output').style.visibility = 'hidden';
+
+document.getElementById('kgInput').addEventListener('input', function (e) {
+    document.getElementById('output').style.visibility = 'visible';
+    let kg = e.target.value;
+    document.getElementById('gramsOutput').innerHTML = kg * 1000;
+    document.getElementById('lbsOutput').innerHTML = kg * 2.2046;
+    document.getElementById('ozOutput').innerHTML = kg * 35.2739;
+})
